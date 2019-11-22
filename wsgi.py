2b5b8e544289470a5f3f6711b3ba06d3b2eedbb4
AntonioC73 <antoniocastellucci@gmail.com>
2019-11-22 18:28:30 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello AC-World!"
+    return "Hello AC-v2-World!"
 
 if __name__ == "__main__":
     application.run()
